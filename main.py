81e8cf59a5ba20994f484c4410a3d31102eb0401
Jakubpaldyna <jakub.paldyna03@wp.pl>
2024-06-22 18:05:19 +0200

    dodano funkcje

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import folium
+import webbrowser
+
 # Dane
-klienci = ["Jan Kowalski", "Anna Nowak", "Piotr Wiśniewski"]
-kierowcy = ["Marcin Kowalski", "Tomasz Nowak", "Andrzej Wiśniewski"]
-taksowki = ["T123", "T124", "T125"]
+klienci = [("Jan Kowalski", "52.2297,21.0122"), ("Anna Nowak", "52.2298,21.0123"),
+           ("Piotr Wiśniewski", "52.2299,21.0124")]
+kierowcy = [("Marcin Kowalski", "52.2303,21.0128"), ("Tomasz Nowak", "52.2304,21.0129"),
+            ("Andrzej Wiśniewski", "52.2305,21.0130")]
+taksowki = [("T123", "52.2297,21.0122"), ("T124", "52.2298,21.0123"), ("T125", "52.2299,21.0124")]
 rezerwacje = []
 
 
 # Funkcje
 
-def dodaj_do_listy(lista, item):
-    lista.append(item)
+def dodaj_do_listy(lista, item, coords):
+    lista.append((item, coords))
     print("Dodano element do listy!")
 
 
@@ -16,27 +21,30 @@ def wyswietl_liste(lista):
     if not lista:
         print("Lista jest pusta!")
     else:
-        print("\n".join(lista))
+        for item, coords in lista:
+            print(f"{item} - {coords}")
 
 
 def usun_element(lista):
     item = input("Podaj element do usunięcia: ")
-    if item in lista:
-        lista.remove(item)
-        print("Usunięto element z listy!")
-    else:
-        print("Element nie znaleziony!")
+    for i, (elem, coords) in enumerate(lista):
+        if elem == item:
+            del lista[i]
+            print("Usunięto element z listy!")
+            return
+    print("Element nie znaleziony!")
 
 
 def aktualizuj_element(lista):
     old_item = input("Podaj element do aktualizacji: ")
-    if old_item in lista:
-        new_item = input("Podaj nową wartość: ")
-        index = lista.index(old_item)
-        lista[index] = new_item
-        print("Zaktualizowano element!")
-    else:
-        print("Element nie znaleziony!")
+    for i, (elem, coords) in enumerate(lista):
+        if elem == old_item:
+            new_item = input("Podaj nową wartość: ")
+            new_coords = input("Podaj nowe współrzędne: ")
+            lista[i] = (new_item, new_coords)
+            print("Zaktualizowano element!")
+            return
+    print("Element nie znaleziony!")
 
 
 def wyswietl_klientow_dla_taksowki():
@@ -55,6 +63,31 @@ def dodaj_rezerwacje():
     klient = input("Podaj imię klienta: ")
     rezerwacje.append((taksowka, dzien, klient))
     print("Dodano rezerwację!")
+
+
+def generuj_mape():
+    mapa = folium.Map(location=[52.2297, 21.0122], zoom_start=12)
+
+    for klient, coords in klienci:
+        lat, lon = map(float, coords.split(","))
+        folium.Marker([lat, lon], popup=f"Klient: {klient}").add_to(mapa)
+
+    for kierowca, coords in kierowcy:
+        lat, lon = map(float, coords.split(","))
+        folium.Marker([lat, lon], popup=f"Kierowca: {kierowca}").add_to(mapa)
+
+    for taksowka, coords in taksowki:
+        lat, lon = map(float, coords.split(","))
+        folium.Marker([lat, lon], popup=f"Taksówka: {taksowka}").add_to(mapa)
+
+    mapa.save("mapa.html")
+    print("Mapa została wygenerowana i zapisana jako 'mapa.html'.")
+
+    # Spróbuj otworzyć mapę w przeglądarce
+    try:
+        webbrowser.open("mapa.html")
+    except Exception as e:
+        print(f"Nie udało się otworzyć mapy automatycznie. Otwórz plik 'mapa.html' ręcznie. Błąd: {e}")
 
 
 # Logowanie
@@ -86,12 +119,13 @@ def otworz_panel_glowny():
         print("12. Aktualizuj taksówkę")
         print("13. Dodaj rezerwację")
         print("14. Wyświetl klientów dla taksówki")
+        print("15. Generuj mapę")
         print("0. Wyjście")
 
         choice = input("Wybierz opcję: ")
 
         if choice == "1":
-            dodaj_do_listy(klienci, input("Podaj imię klienta: "))
+            dodaj_do_listy(klienci, input("Podaj imię klienta: "), input("Podaj współrzędne klienta: "))
         elif choice == "2":
             wyswietl_liste(klienci)
         elif choice == "3":
@@ -99,7 +133,7 @@ def otworz_panel_glowny():
         elif choice == "4":
             aktualizuj_element(klienci)
         elif choice == "5":
-            dodaj_do_listy(kierowcy, input("Podaj imię kierowcy: "))
+            dodaj_do_listy(kierowcy, input("Podaj imię kierowcy: "), input("Podaj współrzędne kierowcy: "))
         elif choice == "6":
             wyswietl_liste(kierowcy)
         elif choice == "7":
@@ -107,7 +141,7 @@ def otworz_panel_glowny():
         elif choice == "8":
             aktualizuj_element(kierowcy)
         elif choice == "9":
-            dodaj_do_listy(taksowki, input("Podaj numer taksówki: "))
+            dodaj_do_listy(taksowki, input("Podaj numer taksówki: "), input("Podaj współrzędne taksówki: "))
         elif choice == "10":
             wyswietl_liste(taksowki)
         elif choice == "11":
@@ -118,6 +152,8 @@ def otworz_panel_glowny():
             dodaj_rezerwacje()
         elif choice == "14":
             wyswietl_klientow_dla_taksowki()
+        elif choice == "15":
+            generuj_mape()
         elif choice == "0":
             break
         else:
